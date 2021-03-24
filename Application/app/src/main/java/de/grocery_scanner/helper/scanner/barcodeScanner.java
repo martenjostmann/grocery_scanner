2c9face1f8de5e88a918eaecae9eb07c2f10b6ef
m_jost02 <marten.jostmann@uni-muenster.de>
2021-03-24 13:57:18 +0100

    resolved double scan bug + design updates

--- Application/app/src/main/java/de/grocery_scanner/helper/scanner/barcodeScanner.java
+++ Application/app/src/main/java/de/grocery_scanner/helper/scanner/barcodeScanner.java
@@ -1,7 +1,9 @@
 package de.grocery_scanner.helper.scanner;
 
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Intent;
@@ -15,8 +17,11 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.VolleyError;
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
@@ -47,6 +52,8 @@ public class barcodeScanner extends AppCompatActivity {
     //This class provides methods to play DTMF tones
     private ToneGenerator toneGen1;
     private TextView barcodeText;
+    private TextView addedNumber;
+    private LinearLayout bottomNav;
     private String barcodeData;
 
     private String barCode;
@@ -61,6 +68,8 @@ public class barcodeScanner extends AppCompatActivity {
         toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC,100);
         surfaceView = findViewById(R.id.surface_view);
         barcodeText = findViewById(R.id.barcode_text);
+        addedNumber = findViewById(R.id.addedNumber);
+        bottomNav = findViewById(R.id.bottomNav);
 
         initialiseDetectorsAndSources();
 
@@ -125,89 +134,116 @@ public class barcodeScanner extends AppCompatActivity {
 
 
         barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            Boolean activation = false;
+
             @Override
-            public void release() {
-                // Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
+            public synchronized void release() {
+                Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
+                cameraSource.stop();
+                activation = true;
             }
 
             @Override
             public void receiveDetections(Detector.Detections<Barcode> detections) {
-                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                    final SparseArray<Barcode> barcodes = detections.getDetectedItems();
 
 
                 if (barcodes.size() != 0) {
+
                     ExecutorService pool = Executors.newFixedThreadPool(3);
                     Future<String> future = pool.submit(new Callable<String>(){
 
                         @Override
-                        public String call() throws Exception {
-                            if (barcodes.valueAt(0).email != null) {
-                                barcodeText.removeCallbacks(null);
-                                barcodeData = barcodes.valueAt(0).email.address;
-                                barcodeText.setText(barcodeData);
-                                toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150);
-                            } else {
+                        public synchronized String call() throws Exception {
+                            if(!activation) {
+
                                 barcodeData = barcodes.valueAt(0).displayValue;
-                                barcodeText.setText(barcodeData);
+
                                 toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150);
+                                activation = true;
+                                return barcodeData;
+                            }else{
+                                return null;
                             }
-                            return barcodeData;
+
+
                         }
                     });
 
-                    try {
+                    if(!activation) {
+                        try {
 
-                        barCode = future.get();
+                            barCode = future.get();
 
-                        Handler handler = new Handler(Looper.getMainLooper());
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                cameraSource.release();
-                            }
-                        });
+                            AppDatabase database = new instantiateDatabase().getDatabase(getApplicationContext());
+                            eanDAO = database.getEanDAO();
 
-                        AppDatabase database = new instantiateDatabase().getDatabase(getApplicationContext());
-                        eanDAO = database.getEanDAO();
+                            int countEntries = eanDAO.checkEan(barCode);
 
-                        int countEntries = eanDAO.checkEan(barCode);
+                            if (countEntries != 0) {
+                                String productName = eanDAO.getItemById(barCode).getName();
+                                barcodeText.setText(productName);
 
-                        if(countEntries != 0){
+                            } else {
+                                eanDatabase eanD = new eanDatabase(barCode, "http://opengtindb.org/", getApplicationContext());
+                                eanD.getProduct(new VolleyCallback() {
+                                    @Override
+                                    public void onSuccessResponse(String result) {
 
-                        }else{
-                            eanDatabase eanD = new eanDatabase(barCode,"http://opengtindb.org/", getApplicationContext());
-                            eanD.getProduct(new VolleyCallback() {
-                                @Override
-                                public void onSuccessResponse(String result) {
-                                    Log.d("TAG", "onSuccessResponse: " + result);
+                                        if (result != null) {
+                                            barcodeText.setText(result);
 
-                                    if(result != null) {
-                                        ean newEan = new ean();
-                                        newEan.setEanId(barCode);
-                                        newEan.setName(result);
-                                        eanDAO.insert(newEan);
-                                    }else{
-                                        Intent insertEanIntent = new Intent(getApplicationContext(), insertEan.class);
-                                        insertEanIntent.putExtra("ean", barCode);
-                                        startActivity(insertEanIntent);
+                                            //insert product into database
+                                            ean newEan = new ean();
+                                            newEan.setEanId(barCode);
+                                            newEan.setName(result);
+                                            eanDAO.insert(newEan);
+                                        } else {
+                                            insertEan();
+                                        }
                                     }
-                                }
-                            });
 
-                            Log.d("TAG", "test: " + ApiResult);
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        insertEan();
+                                        Log.d("Error", "onErrorResponse: " + error);
+                                    }
+                                });
 
+                            }
+
+                            int increaseNumber = Integer.parseInt(addedNumber.getText().toString()) + 1;
+                            addedNumber.setText("" + increaseNumber);
+
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
                         }
+                    }
 
-                    } catch (ExecutionException e) {
-                        e.printStackTrace();
+
+                    bottomNav.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.red));
+                    try {
+                        Thread.sleep(800);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-
+                    activation = false;
+                    bottomNav.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary));
                 }
             }
         });
     }
 
+    private void insertEan(){
+        Intent insertEanIntent = new Intent(getApplicationContext(), insertEan.class);
+        insertEanIntent.putExtra("ean", barCode);
+        startActivity(insertEanIntent);
+    }
 
+    private void insertInventory(AppDatabase database){
+        
+    }
 }
